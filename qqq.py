87ff9ba774869de8379e6828ca43328a418f9ab8
fakeface2117 <90862261+fakeface2117@users.noreply.github.com>
2025-03-15 00:04:09 +0300

    Create qqq.py

---
+++ qqq.py
@@ -0,0 +1,32 @@
+import asyncio
+from typing import Final
+
+from aiohttp import ClientSession
+from fastapi import Depends, FastAPI
+from starlette.requests import Request
+
+app: Final = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    setattr(app.state, "client_session", ClientSession(raise_for_status=True))
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await asyncio.wait((app.state.client_session.close()), timeout=5.0)
+
+
+def client_session_dep(request: Request) -> ClientSession:
+    return request.app.state.client_session
+
+
+@app.get("/")
+async def root(
+    client_session: ClientSession = Depends(client_session_dep),
+) -> str:
+    async with client_session.get(
+        "https://example.com/", raise_for_status=True
+    ) as the_response:
+        return await the_response.text()
